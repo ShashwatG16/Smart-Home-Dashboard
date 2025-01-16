dd6f6c6e8fdae66d3badcc4739a31c8df7d20dda
Shashwat Kumar Gautam <iit2020030@iiita.ac.in>
2025-01-16 23:39:29 +0530

    first commit

---
+++ SHD-backend/src/main/java/com/example/SmartHomeDashboard/SHD/ShdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartHomeDashboard.SHD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShdApplication.class, args);
+	}
+
+}
---
+++ SHD-backend/src/test/java/com/example/SmartHomeDashboard/SHD/ShdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartHomeDashboard.SHD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
